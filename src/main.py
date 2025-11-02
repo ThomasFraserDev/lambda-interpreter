f21863800ee5532737890714c4bfa568db0a74f8
Thomas Fraser <8thomas0fraser8@gmail.com>
2025-11-02 21:19:17 +0000

    - Implemented substitution functionality - Added a command to show an expression - Added a command to list all expressions

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from ast import Variable, Abstraction, Application
+from substitution import substitute
 
 def parse(expr):
     expr = expr.replace("//", "λ")
     if expr.startswith("λ"):
         param = expr[1]
-        body = expr[3:]
+        body = Variable(expr[3:])
         return Abstraction(param, body)
+    return Variable(expr)
 
 print("Command List:")
-print("     CreateVar <name>    -> create a variable")
-print("     CreateExpr <expr>   -> create an expression (you may use // to represent λ)")
-print("     Apply <t1> <t2>     -> apply two terms together")
-print("     Quit                -> exit")
+print("     CreateVar <name>")
+print("     CreateExpr <expr>")
+print("     Apply <t1> <t2>")
+print("     Substitute <t1> <var> <replacement>")
+print("     Show <name>")
+print("     List")
+print("     Quit")
 
 terms = {}
 exprCounter = 0
@@ -44,6 +49,30 @@ while True:
             appTerms = Application(t1, t2)
             terms[name] = appTerms
             print(f"Expression '{terms[name]}' created.")
+    
+    elif params[0] == "substitute" and len(params) == 4:
+        term_name, var, repl_name = params[1], params[2], params[3]
+        term = terms[term_name]
+        replacement = terms[repl_name]
+        exprCounter += 1
+        name = f"expr{exprCounter}"
+        terms[name] = substitute(term, var, replacement)
+        print(f"Substitution '{name}' = {terms[name]} created.")
+        
+            
+    elif params[0] == "show" and len(params) == 2:
+        name = params[1]
+        if terms[name]:
+            print(f"{terms[name]}")
+        else:
+            print(f"No expression found with that name.")
+            
+    elif params[0] == "list" and len(params) == 1:
+        if terms:
+            for name, expr in terms.items():
+                print(f"{name}: {expr}")
+        else:
+            print("No expressions have been defined.")
 
     else:
         print("Unknown command or number of arguments.")
